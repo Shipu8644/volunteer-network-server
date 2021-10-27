e220f42661a3c005c135d476b3577714f8912d2b
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-28 03:11:46 +0600

    single data showing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,6 +24,15 @@ async function run() {
             const cursor = serviceCollection.find({});
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        //get single Api
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            console.log(id);
+            res.send(service);
         })
 
 
